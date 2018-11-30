4af054ad97925d78c3192b0536a3e3d309e85597
NotLiu <Andrew Liu>
2018-11-30 18:59:58 +0800

    Create Tic-Tac.py

---
+++ Tic-Tac.py
@@ -0,0 +1,11 @@
+#------------------------------
+#File:      Tic-Tac.py
+#------------------------------
+
+import pygame
+
+def place_mark():
+    if pygame.mouse.get_pressed() == (1,0,0):
+        if pygame.mouse.get_pos()[0] < 100 and pygame.mouse.get_pos()[1]<100:
+
+def 
